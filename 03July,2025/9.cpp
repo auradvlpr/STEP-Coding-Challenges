1cca09b352add677265506fa1a4efb16384121ce
Aura Bhattacharyya <149257111+auradvlpr@users.noreply.github.com>
2025-07-03 19:23:44 +0530

    Patterns

---
+++ 03July,2025/9.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void printpattern9_1(int m){
+    for(int i=0; i<m; i++){
+        //space
+        for(int j=0; j<m-i-1; j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0; j<2*i+1; j++){
+            cout<<"*";
+        }//space
+        for(int j=0; j<m-i-1; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void printpattern9_2(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0; j<2*n-(2*i+1); j++){
+            cout<<"*";
+        }//space
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    int a, b;
+    cin>>a>>b;
+    printpattern9_1(a);
+    printpattern9_2(b);
+}
