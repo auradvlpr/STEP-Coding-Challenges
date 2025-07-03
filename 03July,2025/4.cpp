ce3eee6d3976efaa7596e6e1bf0647ae83e571e9
Aura Bhattacharyya <149257111+auradvlpr@users.noreply.github.com>
2025-07-03 18:57:02 +0530

    Patterns

--- 03July,2025/4.cpp
+++ 03July,2025/4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void printpattern3(int n){
+void printpattern4(int n){
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
             cout<<i;
@@ -13,5 +13,5 @@ void printpattern3(int n){
 int main() {
     int a;
     cin>>a;
-    printpattern3(a);
+    printpattern4(a);
 }
